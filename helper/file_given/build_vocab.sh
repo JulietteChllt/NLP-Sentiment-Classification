750049df538eb6a0f72e4a897b40cdc90e3c6ad0
JulietteChllt <63093857+JulietteChllt@users.noreply.github.com>
2021-12-22 16:25:16 +0100

    created folder with teacher given files

--- helper/file_given/build_vocab.sh
+++ helper/file_given/build_vocab.sh
@@ -3,4 +3,4 @@
 # Note that this script uses GNU-style sed as gsed. On Mac OS, you are required to first https://brew.sh/
 #    brew install gnu-sed
 # on linux, use sed instead of gsed in the command below:
-cat twitter-datasets/train_pos_full.txt twitter-datasets/train_neg_full.txt | gsed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
+cat train_pos_full.txt train_neg_full.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
